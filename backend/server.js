1adfcc00657602b99536cfa906c150ddf2f03f7c
Antonio-Mora003 <antoniomor42003@gmail.com>
2025-02-27 19:52:51 -0500

    Corrección de puerto y ajustes en el servidor

--- backend/server.js
+++ backend/server.js
@@ -10,15 +10,27 @@ app.use(express.json());
 app.use(cors());
 
 // Conectar a MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("MongoDB conectado"))
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB conectado"))
   .catch(err => console.error(err));
 
 // Rutas
 app.use("/api/users", require("./routes/userRoutes"));
 
 // Iniciar servidor
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
+
+app.get("/", (req, res) => {
+    res.send("Servidor funcionando correctamente");
+});
+const path = require("path");
+
+// Servir archivos estáticos
+app.use(express.static(path.join(__dirname, "frontend")));
+
+// Servir el archivo index.html en la raíz
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
+
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
